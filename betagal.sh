2ee47f33ddddcff1cd74f4e9bc3bc777bef301a3
Ruben Meana-Paneda <ruben.meana-paneda@nih.gov>
2026-06-18 15:17:15 -0400

    reverted codebase to previous commit

--- betagal.sh
+++ betagal.sh
@@ -6,15 +6,15 @@ simple_exec prg=import_movies cs=1.4 fraca=0.1 kv=200 smpd=0.885 filetab=movies.
 echo " >>> PROGRAM: motion_correct" >> LOG
 simple_exec prg=motion_correct nparts=5 nthr=8 gainref=/mnt/beegfs/elmlund/testing-datasets/betagal/gain/gain.mrc total_dose=30.65 smpd_downscale=1.3 >> LOG
 echo " >>> PROGRAM: ctf_estimate" >> LOG
-simple_exec prg=ctf_estimate nparts=5 nthr=8 projfile=2_motion_correct/betagal.simple >> LOG
+simple_exec prg=ctf_estimate nparts=1 nthr=16 >> LOG
 filetab_mrc.pl 2_motion_correct/
 echo " >>> PROGRAM: pick" >> LOG
-simple_exec prg=pick picker=segdiam projfile=3_ctf_estimate/betagal.simple nparts=5 nthr=8 >> LOG
+simple_exec prg=pick picker=segdiam nparts=5 nthr=8 >> LOG
 echo " >>> PROGRAM: extract" >> LOG
-simple_exec prg=extract box=256 nparts=5 nthr=8 projfile=4_pick/betagal.simple >> LOG
-echo " >>> PROGRAM: abinitio2D" >> LOG
-simple_exec prg=abinitio2D ncls=100 mskdiam=190 nparts=4 nthr=16 projfile=5_extract/betagal.simple >> LOG 
+simple_exec prg=extract box=256 nparts=8 nthr=8 >> LOG
 echo " >>> PROGRAM: selection" >> LOG
-simple_exec prg=selection res_threshold=7 oritype=cls2D projfile=6_abinitio2D/betagal.simple >> LOG
+simple_exec prg=selection res_threshold=7 >> LOG
+echo " >>> PROGRAM: abinitio2D" >> LOG
+simple_exec prg=abinitio2D ncls=100 mskdiam=190 nthr=24 >> LOG 
 echo " >>> PROGRAM: abinitio3D" >> LOG
-simple_exec prg=abinitio3D pgrp=d2 mskdiam=190 nparts=4 nthr=16 projfile=7_selection/betagal.simple >> LOG
+simple_exec prg=abinitio3D pgrp=d2 mskdiam=190 nthr=40 >> LOG
