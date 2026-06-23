ae546936d5f088aaf86d2c9102bb402ef2da696f
Ruben Meana-Paneda <ruben.meana-paneda@nih.gov>
2026-06-23 15:00:53 -0400

    add apoferritin to the data testing

--- apof.sh
+++ apof.sh
@@ -1,8 +1,35 @@
 simple_exec prg=new_project projname=apof
-cd apof
+cd apoferritin_subset/
 filetab_movs.pl /mnt/beegfs/elmlund/testing-datasets/apoferritin/20221214_105239_vitroEase_apoF_bf15_300kv_highres/movies
-simple_exec prg=import_movies filetab=movies.txt smpd=0.693 ctf=yes cs=2.7 kv=300 fraca=0.1
-simple_exec prg=motion_correct smpd_downscale=1.3 total_dose=51.8 nparts=2 nthr=10 projfile=1_import_movies/apof.simple gainref=/mnt/beegfs/elmlund/testing-datasets/apoferritin/20221214_105239_vitroEase_apoF_bf15_300kv_highres/gain/20221214_114106_EER_GainReference.gain > MOTION_CORRECTION
+echo " >>> PROGRAM: import_movies" > LOG
+simple_exec prg=import_movies cs=2.7 fraca=0.1 kv=300 smpd=0.693 filetab=movies.txt
+echo " >>> PROGRAM: motion_correct" >> LOG
+simple_exec prg=motion_correct nparts=5 nthr=8 gainref=/mnt/beegfs/elmlund/testing-datasets/apoferritin/20221214_105239_vitroEase_apoF_bf15_300kv_highres/20221214_114106_EER_GainReference.gain total_dose=51.8 smpd_downscale=1.3
+echo " >>> PROGRAM: ctf_estimate" >> LOG
+simple_exec prg=ctf_estimate nparts=5 nthr=8
+simple_exec prg=oristats oritab=3_ctf_estimate/apoferritin_subset.simple nthr=1 ctfstats=yes oritype=mic
+#simple_exec prg=mini_stream cs=2.7 fraca=0.1 kv=300 smpd=1.3 filetab=filetab.txt nthr=24
+#simple_exec prg=convert smpd=1.3 stk=mini_stream_selection.spi outstk=mini_stream_selection.mrc
+#simple_exec prg=pick pickrefs=mini_stream_selection.mrc nparts=5 nthr=8 projfile=3_ctf_estimate/apof.simple
+echo " >>> PROGRAM: pick" >> LOG
+simple_exec prg=pick picker=segdiam nparts=5 nthr=8 projfile=3_ctf_estimate/apof.simple
+echo " >>> PROGRAM: extract" >> LOG
+simple_exec prg=extract box=192 projfile=5_pick/apof.simple nparts=8 nthr=8
+wc 5_pick/*box
+echo " >>> PROGRAM: abinitio2D" >> LOG
+simple_exec prg=abinitio2D ncls=50 mskdiam=160 nthr=24
+echo " >>> PROGRAM: selection" >> LOG
+simple_exec prg=selection res_threshold=9 oritype=cls2D projfile=6_abinitio2D/apof.simple >> LOG
+#simple_exec prg=map_cavgs_selection stk2=abinitio2d_selected.spi
+echo " >>> PROGRAM: abinitio3D" >> LOG
+simple_exec prg=abinitio3D pgrp=o mskdiam=160 nthr=40
+
+
+#simple_exec prg=new_project projname=apof
+#cd apof
+#filetab_movs.pl /mnt/beegfs/elmlund/testing-datasets/apoferritin/20221214_105239_vitroEase_apoF_bf15_300kv_highres/movies
+#simple_exec prg=import_movies filetab=movies.txt smpd=0.693 ctf=yes cs=2.7 kv=300 fraca=0.1
+#simple_exec prg=motion_correct smpd_downscale=1.3 total_dose=51.8 nparts=2 nthr=10 projfile=1_import_movies/apof.simple gainref=/mnt/beegfs/elmlund/testing-datasets/apoferritin/20221214_105239_vitroEase_apoF_bf15_300kv_highres/gain/20221214_114106_EER_GainReference.gain > MOTION_CORRECTION
 #simple_exec prg=motion_correct smpd_downscale=1.3 total_dose=51.8 nparts=2 nthr=10 script=yes projfile=1_import_movies/apof.simple gainref=/mnt/beegfs/elmlund/testing-datasets/apoferritin/20221214_105239_vitroEase_apoF_bf15_300kv_highres/gain/20221214_114106_EER_GainReference.gain > MOTION_CORRECTION
 #simple_exec prg=ctf_estimate projfile=2_motion_correct/apof.simple nparts=8 nthr=8 script=yes
 #simple_exec prg=oristats oritab=3_ctf_estimate/apof.simple ctfstats=yes nthr=8 oritype=mic
