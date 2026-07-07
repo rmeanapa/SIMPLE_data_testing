0610f8bf6ae097294cd100558e32dadcfe28eebc
Ruben Meana-Paneda <ruben.meana-paneda@nih.gov>
2026-07-07 11:38:34 -0400

    typo

--- betagal.sh
+++ betagal.sh
@@ -19,4 +19,4 @@ simple_exec prg=selection res_threshold=9 oritype=cls2D projfile=6_abinitio2D/be
 echo " >>> PROGRAM: abinitio3D" >> LOG
 simple_exec prg=abinitio3D pgrp=d2 mskdiam=190 nthr=32 projfile=7_selection/betagal.simple >> LOG
 echo " >>> PROGRAM: flex_eigenvol" >> LOG
-simple_exec prg=flex_eigenvol vo1l=8_abinitio3D/rec_final_state01_lp.mrc nthr=32 projfile=8_abinitio3D/betagal.simple >> LOG
+simple_exec prg=flex_eigenvol vol1=8_abinitio3D/rec_final_state01_lp.mrc nthr=32 projfile=8_abinitio3D/betagal.simple >> LOG
