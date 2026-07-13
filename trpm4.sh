42698876b415c3de2b042e3d760e1fc12fe6f89b
Ruben Meana-Paneda <ruben.meana-paneda@nih.gov>
2026-07-13 18:11:52 -0400

    add trpm4

--- trpm4.sh
+++ trpm4.sh
@@ -1,29 +1,27 @@
-name=trpm4
-smpd=0.732
-dose=55
-dir=/mnt/beegfs/elmlund/testing-datasets/TRPM4/20231113_161724_83_hsTRPM4_BA_5mgml_2s_m10
-gain=$dir/gain/$(ls $dir/gain)
+simple_exec prg=new_project projname=trpm4  > LOG
+cd trpm4 
+filetab_movs.pl /mnt/beegfs/elmlund/testing-datasets/TRPM4/20231113_161724_83_hsTRPM4_BA_5mgml_2s_m10/movies 50
+echo " >>> PROGRAM: import_movies" > LOG
+simple_exec prg=import_movies cs=2.7 fraca=0.1 kv=300 smpd=0.732 filetab=movies.txt >> LOG
+echo " >>> PROGRAM: motion_correct" >> LOG
+simple_exec prg=motion_correct nparts=5 nthr=8 gainref=/mnt/beegfs/elmlund/testing-datasets/TRPM4/20231113_161724_83_hsTRPM4_BA_5mgml_2s_m10/gain/20231019_151343_EER_GainReference.gain total_dose=55 smpd_downscale=1.3 >> LOG
+echo " >>> PROGRAM: ctf_estimate" >> LOG
+simple_exec prg=ctf_estimate nparts=5 nthr=8 projfile=2_motion_correct/trpm4.simple >> LOG
+filetab_mrc.pl 2_motion_correct/
+echo " >>> PROGRAM: oristats" >> LOG
+simple_exec prg=oristats oritab=3_ctf_estimate/trpm4.simple nthr=1 ctfstats=yes oritype=mic >> LOG
+echo " >>> PROGRAM: selection" >> LOG
+simple_exec prg=selection projfile=3_ctf_estimate/trpm4.simple ctfresthreshold=7 icefracthreshold=1 oritype=mic >> LOG
+echo " >>> PROGRAM: print_project" >> LOG
+simple_exec prg=print_project_field oritype=mic projfile=4_selection/trpm4.simple | awk '{print $6}' | awk -F'=' '{print $2}' > ctf_icefrac_selection.txt 
+echo " >>> PROGRAM: pick" >> LOG
+simple_exec prg=pick picker=segdiam projfile=3_ctf_estimate/trpm4.simple nparts=5 nthr=8 >> LOG
+echo " >>> PROGRAM: extract" >> LOG
+simple_exec prg=extract box=256 nparts=5 nthr=8 projfile=5_pick/trpm4.simple >> LOG
+echo " >>> PROGRAM: abinitio2D" >> LOG
+simple_exec prg=abinitio2D ncls=100 mskdiam=190 nthr=32 projfile=6_extract/trpm4.simple >> LOG 
+echo " >>> PROGRAM: selection" >> LOG
+simple_exec prg=selection res_threshold=9 oritype=cls2D projfile=7_abinitio2D/trpm4.simple >> LOG
+echo " >>> PROGRAM: abinitio3D" >> LOG
+simple_exec prg=abinitio3D pgrp=d2 mskdiam=190 nthr=32 projfile=8_selection/trpm4.simple >> LOG
 
-simple_exec prg=new_project projname=$name qsys_partition=csbdevel
-cd $name
-find $dir/movies -type f > movies.txt
-simple_exec prg=import_movies smpd=$smpd cs=2.7 kv=300 fraca=0.1 filetab=movies.txt
-simple_exec prg=motion_correct gainref=$gain total_dose=$dose nparts=32 nthr=6 projfile=1_import_movies/$name.simple script=yes
-simple_exec prg=ctf_estimate projfile=2_motion_correct/$name.simple nparts=16 nthr=8 script=yes
-simple_exec prg=oristats oritab=3_ctf_estimate/$name.simple ctfstats=yes nthr=8 oritype=mic
-AVERAGE CTF RESOLUTION               :     4.99
-STANDARD DEVIATION OF CTF RESOLUTION :     5.40
-MINIMUM CTF RESOLUTION (BEST)        :     3.02
-MAXIMUM CTF RESOLUTION (WORST)       :    50.00
-AVERAGE DF                           :     0.52
-STANDARD DEVIATION OF DF             :     0.33
-MINIMUM DF                           :     0.20
-MAXIMUM DF                           :     4.86
-simple_exec prg=selection oritype=mic projfile=3_ctf_estimate/$name.simple ctfresthreshold=7 icefracthreshold=1
-simple_exec prg=selection oritype=mic projfile=3_ctf_estimate/$name.simple ctfresthreshold=7 icefracthreshold=1 nran=300
-simple_exec prg=print_project_field oritype=mic projfile=5_selection/$name.simple > tmp.txt
-awk '{print $6}' tmp.txt > tmp2.txt
-awk -F'=' '{print $2}' tmp2.txt > sel5.txt
-simple_exec prg=mini_stream script=yes cs=2.7 kv=300 smpd=1.3 nthr=18 filetab=sel5.txt projfile=$name.simple
-
-simple_exec prg=extract nparts=8 nthr=6 box=180 projfile=7_pick/$name.simple script=yes
