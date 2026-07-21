2cae3593ea04d4b2b200c68b5ac706ba691999aa
Ruben Meana-Paneda <ruben.meana-paneda@nih.gov>
2026-07-21 17:18:29 -0400

    minor edit

--- nanox.sh
+++ nanox.sh
@@ -1,7 +1,7 @@
 simple_exec prg=new_project projname=nanox  > LOG
 cd nanox 
-filetab_movs.pl /mnt/beegfs/elmlund/NanoX/full_frame_data_sets/71-17/mrc 
-echo " >>> PROGRAM: tseries_import" > LOG
+filetab_movs.pl /mnt/beegfs/elmlund/NanoX/full_frame_data_sets/71-17/mrc >> LOG
+echo " >>> PROGRAM: tseries_import" >> LOG
 single_exec prg=tseries_import filetab=movies.txt cs=-0.01 fraca=0.3 kv=300 smpd=0.358 >> LOG
 echo " >>> PROGRAM: tseries_motion_correct" >> LOG
 single_exec prg=tseries_motion_correct nparts=6 nthr=12 >> LOG 
@@ -21,7 +21,7 @@ simple_exec prg=import_particles cs=-0.01 fraca=0.3 kv=300 smpd=0.358 stk=../ppc
 echo " >>> PROGRAM: analysis2D_nano" >> ../../../LOG
 single_exec prg=analysis2D_nano element=Pt nthr=16 >> ../../../LOG
 #echo " >>> PROGRAM: map_cavgs_selection" >> LOG
-#single_exec prg=map_cavgs_selection stk2=2_analysis2D_nano/selected.spi prune=yes >> LOG
+#simple_exec prg=map_cavgs_selection stk2=2_analysis2D_nano/selected.spi prune=yes >> LOG
 echo " >>> PROGRAM: autorefine3D_nano" >> ../../../LOG
 #single_exec prg=autorefine3D_nano vol1=startvol.mrc element=Pt smpd=0.358 pgrp=c1 lp=1.5 mskdiam=30 nthr=16 projfile=3_selection/tracked_ptcl_1.simple >> LOG 
 single_exec prg=autorefine3D_nano vol1=2_analysis2D_nano/startvol.mrc element=Pt smpd=0.358 pgrp=c1 lp=1.5 mskdiam=30 nthr=16 projfile=2_analysis2D_nano/tracked_ptcl_1.simple >> ../../../LOG
