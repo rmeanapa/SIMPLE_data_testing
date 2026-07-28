1597b91d5ef6c734048853143e8fa86b4b6590ea
Ruben Meana-Paneda <ruben.meana-paneda@nih.gov>
2026-07-28 08:49:55 -0400

    remove simulated workflows

--- trpm4.sh
+++ trpm4.sh
@@ -7,7 +7,7 @@ echo " >>> PROGRAM: motion_correct" >> LOG
 simple_exec prg=motion_correct nparts=5 nthr=8 gainref=/mnt/beegfs/elmlund/testing-datasets/TRPM4/20231113_161724_83_hsTRPM4_BA_5mgml_2s_m10/gain/20231019_151343_EER_GainReference.gain total_dose=55 smpd_downscale=1.3 >> LOG
 echo " >>> PROGRAM: ctf_estimate" >> LOG
 simple_exec prg=ctf_estimate nparts=5 nthr=8 projfile=2_motion_correct/trpm4.simple >> LOG
-filetab_mrc.pl 2_motion_correct/
+filetab_mrc.pl 2_motion_correct/ >> LOG
 echo " >>> PROGRAM: oristats" >> LOG
 simple_exec prg=oristats oritab=3_ctf_estimate/trpm4.simple nthr=1 ctfstats=yes oritype=mic >> LOG
 echo " >>> PROGRAM: selection" >> LOG
